a4d2d61251993b58e93e104e2eda3b6082ce63f1
MaelPERON <maelperonmessagerie2015@gmail.com>
2024-12-12 10:56:18 +0100

    Add progress report and user feedbacks

--- operators/snapshot.py
+++ operators/snapshot.py
@@ -72,8 +72,15 @@ class ImportSnapshot(bpy.types.Operator, ImportHelper):
         return {"RUNNING_MODAL"}
     
     def execute(self, context):
+        l = len(self.files)
         new_objs = []
-        for file in self.files:
+        
+        ## Importing FBX files
+        self.report({"INFO"}, "Importing FBX files ({}).".format(l))
+        context.window_manager.progress_begin(1, l)
+
+        for n, file in enumerate(self.files):
+            context.window_manager.progress_update(n)
             filepath = self.directory + file.name
             check = fbx_import_check(filepath)
             if check<1:
@@ -85,8 +92,15 @@ class ImportSnapshot(bpy.types.Operator, ImportHelper):
                 self.report({"WARNING"}, f'"{file.name}" is empty. no object found!')
                 continue
             new_objs.append(obj)
+        context.window_manager.progress_end()
 
-        for obj in new_objs:
+        ## Processing all the imported snapshots
+        self.report({"INFO"}, "Processing snapshots")
+        context.window_manager.progress_begin(1, l)
+
+        for n, obj in enumerate(new_objs):
+            context.window_manager.progress_update(n)
+
             bool_expression = "custom_frames != self.tm_version"
 
             def set_driver(driver):
@@ -118,4 +132,7 @@ class ImportSnapshot(bpy.types.Operator, ImportHelper):
             display_type = obj.driver_add("display_type", -1).driver
             set_driver(display_type)
             display_type.expression = f'display(display_hidden) if {bool_expression} else 5'
+
+        context.window_manager.progress_end()
+        self.report({"INFO"}, "Succesfully imported {} snapshots".format(l))
         return {"FINISHED"}
